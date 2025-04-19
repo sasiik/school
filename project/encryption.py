aef0ffe35f6e3d7d871922313e0049acc2032853
sasiik <sstupko31@gmail.com>
2025-04-19 20:36:35 +0300

    easy encrypt changes

--- project/encryption.py
+++ project/encryption.py
@@ -4,7 +4,7 @@ import random
 class Encryptor:
 
     def __init__(self):
-        self.nonce = random.randint(1, 100)
+        self.nonce = random.randint(1, 1000)
 
     def define_encryption_key(self):
         enc_key_raw = input('Enter encryption key: ')
@@ -18,7 +18,7 @@ class Encryptor:
         enc_text = ''
         for i, text_sym in enumerate(text):
             encrypted_sym = ord(
-                text_sym) + ord(enc_key[i] if i < len(enc_key) else enc_key[i % len(enc_key)]) + self.nonce
+                text_sym) + ord(enc_key[i * self.nonce] if i*self.nonce < len(enc_key) else enc_key[i*self.nonce % len(enc_key)])
             enc_text += chr(encrypted_sym)
         return enc_text
 
@@ -26,18 +26,20 @@ class Encryptor:
         decrypted_text = ''
         for i, enc_text_sym in enumerate(enc_text):
             decrypted_sym = ord(
-                enc_text_sym) - ord(enc_key[i] if i < len(enc_key) else enc_key[i % len(enc_key)]) - self.nonce
+                enc_text_sym) - ord(enc_key[i * self.nonce] if i*self.nonce < len(enc_key) else enc_key[i*self.nonce % len(enc_key)])
             decrypted_text += chr(decrypted_sym)
         return decrypted_text
 
 
 if __name__ == '__main__':
     encryptor = Encryptor()
+    nonce = encryptor.nonce
     enc_key = encryptor.define_encryption_key()
     text = encryptor.define_text()
     encrypted_text = encryptor.encrypt_text(text, enc_key)
     decrypted_text = encryptor.decrypt_text(encrypted_text, enc_key)
     print('Text:', text)
     print('Encryption key:', enc_key)
+    print('Nonce:', nonce)
     print('Encrypted text:', encrypted_text)
     print('Decrypted text:', decrypted_text)
